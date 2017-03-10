1b0ddf3c7a9bcb144696fb549cbdb627199b4d31
Sean Van Osselaer <svanosselaer@aconex.com>
2017-03-11 08:31:32 +1100

    Pretty sure the pip install of ansible could have been not sudo :-|

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev python-pip
 
 pip install --upgrade pip
-sudo pip install ansible==2.1.1.0
+pip install ansible==2.1.1.0
 
 ansible-playbook -i "localhost," -c local playbook.yml
 
