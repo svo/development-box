5280aaadc7983a34159228a01c282cc76d3873f6
Sean Van Osselaer <svanosselaer@aconex.com>
2017-03-24 04:43:48 +1100

    Removing intel-graphics-update-tool

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev python-pip
 
-pip install --upgrade pip
-pip install ansible==2.1.1.0
+sudo -H pip install --upgrade pip
+sudo -H pip install ansible==2.1.1.0
 
 ansible-playbook -i "localhost," -c local playbook.yml
 
 sudo apt -y autoremove
 
-sudo intel-graphics-update-tool &
 jetbrains-toolbox &
