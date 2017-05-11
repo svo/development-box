065e325998de59375ecaed88e4c9bf08da0338ff
Sean Van Osselaer <svanosselaer@aconex.com>
2017-05-11 17:06:52 +1000

    Updating ansible in build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ sudo apt-get -y upgrade
 sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev python-pip
 
 sudo -H pip install --upgrade pip
-sudo -H pip install ansible==2.1.1.0
+sudo -H pip install ansible==2.2.2.0
 
 ansible-playbook -i "localhost," -c local playbook.yml
 
 sudo apt -y autoremove
 
-jetbrains-toolbox &
+echo -e "\n\n-----------------------\nNOTES\n-----------------------\nrun: jetbrains-toolbox &"
